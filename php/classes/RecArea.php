0b3815c5ec8553fec3139c1293f2279eace75f85
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-27 08:32:28 -0600

    Named class and created four variables

---
+++ php/classes/RecArea.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joy
+ * Date: 7/27/2018
+ * Time: 8:27 AM
+ */
+
+class RecArea {
+
+   private $recAreaId; //Uuid
+
+   private $recAreaLat; //Float
+
+   private $recAreaLong; //Float
+
+   private $recAreaName; //Varchar
+
+}
